1611e3697dfb1d4ec95344c6af603258f31d1267
Waleed AL-Njlat <waleed.njlaty@gmail.com>
2026-09-19 15:33:13 +0300

    feat: configure local custom vehicle radio playlist

--- moonloader/KurtlarVadisi/config/radio.lua
+++ moonloader/KurtlarVadisi/config/radio.lua
@@ -1,4 +1,28 @@
--- GTA SA SCM channels: 0..10 = stations, 11 = USER TRACKS, 12 = OFF.
--- Policy only. Future custom songs belong to a separate playlist.
-return { missionOverride = true, offChannel = 12, defaultChannel = 0,
-    settleMs = 750, pollMs = 750 }
+-- Custom in-car radio configuration.
+-- Audio files stay local and are intentionally ignored by Git.
+-- Put the playlist files in:
+--   moonloader\KurtlarVadisi\radio\
+return {
+    missionOverride = true,
+    offChannel = 12,
+    defaultChannel = 0,
+    settleMs = 750,
+    pollMs = 500,
+
+    customEnabled = true,
+    playlistDir = 'moonloader\\KurtlarVadisi\\radio\\',
+    volume = 0.35,
+    retryMs = 5000,
+
+    tracks = {
+        '6Q2MfWVxCeY.m4a',
+        '6Ujs0_z_amI.m4a',
+        'LRvFDvkW1Ro.m4a',
+        'm6Y3n6Wod4g.m4a',
+        'naOnzfjPCcA.m4a',
+        'NgGRTwPmIx4.m4a',
+        'nvhMDDuS09w.m4a',
+        'rfGGUnOPY5c.m4a',
+        '4tf0icuedoI.m4a'
+    }
+}
